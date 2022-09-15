a693fa976d44ada75548ab65bd96a10e46f22807
e1 <e1@e1.com>
2022-09-15 16:57:33 +0300

    Edit Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,3 +1,20 @@
 ﻿int numbA = new Random().Next(1,110);
 int numbB = new Random().Next(1,11);
 Console.Write(numbA/numbB+" ");
+Console.WriteLine();
+
+
+string username = "JohnDow";
+
+while (username!="john") 
+{
+Console.Write("Login: ");
+username = Console.ReadLine();
+
+if (username!="john")
+    {
+        Console.WriteLine("Hello, bastard "+username+"!");
+    }
+    else Console.WriteLine("Hello, lovely John! ");
+
+};
