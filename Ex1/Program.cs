c90fd6da1980002cbdac2cde36f18590667acbee
e1 <e1@e1.com>
2022-09-15 14:33:10 +0300

    edit Ex1 add double

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -6,4 +6,7 @@ Console.WriteLine("Hello, World!");
 
 int numbA = 11;
 int numbB = 3;
-Console.Write(numbA/numbB);
+Console.Write(numbA/numbB+" ");
+double doubleA = 11;
+double doubleB = 3;
+Console.Write(doubleA/doubleB);
